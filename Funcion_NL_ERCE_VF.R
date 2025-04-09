4cdb39e15393f4e206d2aa55421c9541b85a6960
Sadith20 <145497783+Sadith20@users.noreply.github.com>
2025-04-09 12:08:16 -0500

    Funcion_NL_ERCE_VF.R

--- Funcion_NL_ERCE_VF.R
+++ Funcion_NL_ERCE_VF.R
@@ -18,11 +18,6 @@ round2 = function(x, n) {
 # mostrar cifras significaticas (entero + decimal)
 options(digits=8)
 
-# ruta de trabajo
-setwd('F:/SYRA/SYRA_ordenado/Resultados de ERCE y funcion')
-
-base = import('ERCE_2019_QA3 Peru.sav')
-
 NL_ERCE = function(data,estrato,grado,curso,año){
 # cantidad de valores plausibles y prefijo de los pesos replicados
 cant_vp = 5
@@ -129,9 +124,3 @@ for (z in 1:length(unique(data[[estrato]]))) {
 }
   export(resultado_f, paste0('NL_',grado,'_',curso,'_',estrato,'_',año,'.xlsx'), rowNames = TRUE)
 }
-
-NL_ERCE(base,'COUNTRY','3P','Lectura','2019')
-NL_ERCE(base,'SEX','3P','Lectura','2019')
-
-NL_ERCE(base,'COUNTRY','3P','Matematica','2019')
-NL_ERCE(base,'SEX','3P','Matematica','2019')
