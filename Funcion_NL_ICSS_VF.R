dbfa69df84bfd709b0606f96da2fb7d40724f55d
Sadith20 <145497783+Sadith20@users.noreply.github.com>
2025-04-11 17:24:56 -0500

    Funcion_NL_ICSS_VF.R

--- Funcion_NL_ICSS_VF.R
+++ Funcion_NL_ICSS_VF.R
@@ -18,11 +18,6 @@ round2 = function(x, n) {
 # mostrar cifras significaticas (entero + decimal)
 options(digits=8)
 
-# ruta de trabajo
-setwd('E:/SYRA/SYRA_ordenado/Resultados de ICSS y funcion')
-
-base = import('ISGDOMC3.sav')
-
 ICSS_PISA = function(data,estrato,año){
   # cantidad de valores plausibles y prefijo de los pesos replicados
   cant_vp = 5
